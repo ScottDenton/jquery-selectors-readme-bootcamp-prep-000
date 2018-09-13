10b64ae1247fdd17fa806686a12bf01c9c10713b
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-13 17:55:42 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,6 +10,10 @@ function ninjaBabySelector (){
   return $('#baby-ninja')
   }
   
+  function divSelector(){
+    return $('.pics')
+  }
+  
   function firstListItem(){
     return $('#pic-list ul:first-child')
   }
